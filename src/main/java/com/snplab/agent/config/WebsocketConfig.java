bc6cbed724b5ab9f04bd8978dba960e25bac0cc4
chulhyun.ahn <chulhyun.ahn@snplab.io>
2024-09-30 15:24:34 +0900

    agent init

---
+++ src/main/java/com/snplab/agent/config/WebsocketConfig.java
@@ -0,0 +1,21 @@
+package com.snplab.agent.config;
+
+import com.snplab.agent.handler.WebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocket
+public class WebsocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws").setAllowedOrigins("*");
+    }
+}
